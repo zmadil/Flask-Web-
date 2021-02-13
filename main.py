4a930dcd2c1da23856a390aaf69f0b829935ae58
zmadil <zainmadil95@gmail.com>
2021-02-12 23:19:36 -0600

    Added flash messages

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from website import create_app
 
 app= create_app()
 
-if __name__ == '__main__':                #ONly we run main.py, not by importing
+if __name__ == '__main__':                #Only we run main.py, not by importing
     app.run(debug=True)                   #Debug=true means if we make changes, it will automatically rerun the web server
--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,32 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, request     #We import request to get the information that was sent in the form
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods= ['GET','POST'])
 def login():
     return render_template("login.html")
+    
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout </p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods= ['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        email= request.form.get('email')
+        firstName= request.form.get('firstName')
+        password1= request.form.get('password1')
+        password2= request.form.get('password2')
+
+        if len(email) <4:
+            flash ('Email greater than 4 letters', category = 'error')
+        elif len(firstName) <2:
+            flash ('first name greater than 2',category = 'error')
+        elif password1 != password2 :
+            flash ('passwords don\'t match', category = 'error')
+        else:
+            flash ('Account created', category='success')
+            
+
     return render_template("sign_up.html")
